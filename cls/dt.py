817f14509c57d14f080affa12f9f2db1acfb8be7
Yuanhang8605 <liyuan@snerdi.com.cn>
2018-01-17 16:53:32 +0800

    primary decision tree finished

--- cls/dt.py
+++ cls/dt.py
@@ -2,3 +2,37 @@ import pydotplus
 from IPython.display import Image
 from sklearn.metrics import roc_auc_score
 
+from sklearn.tree import DecisionTreeClassifier, export_graphviz
+
+import sys
+sys.path.append('/home/yuan/ml/2018-1-15-essembling_example')
+from data.gen_data import get_train_test
+
+SEED = 222
+
+def print_graph(clf, feature_names):
+   """ print dcision tree. """
+   graph = export_graphviz(
+      clf, 
+      label='root',
+      proportion=True,
+      impurity=False, 
+      out_file=None,
+      feature_names=feature_names,
+      class_names={0: "D", 1: "R"},
+      filled=True,
+      rounded=True
+   )
+
+   graph = pydotplus.graph_from_dot_data(graph)
+   return Image(graph.create_png())
+
+
+# get the training data
+xtrain, xtest, ytrain, ytest = get_train_test()
+
+t1 = DecisionTreeClassifier(max_depth=1, random_state=SEED)
+t1.fit(xtrain, ytrain)
+p = t1.predict_proba(xtest)[:,1]
+
+print "Decision tree ROC_AUC score: %.3f" % roc_auc_score(ytest, p)
--- data/gen_data.py
+++ data/gen_data.py
@@ -1,17 +1,19 @@
 import numpy as np
 import pandas as pd
 import matplotlib.pyplot as plt
-### Import data
-# Always good to set a seed for reproducibility
-SEED = 222
-np.random.seed(SEED)
-df = pd.read_csv('./data/input.csv')
+
 ### Training and test set
 from sklearn.model_selection import train_test_split
 from sklearn.metrics import roc_auc_score
 
 def get_train_test(test_size=0.95):
    """Split Data into train and test sets."""
+   ### Import data
+   # Always good to set a seed for reproducibility
+   SEED = 222
+   np.random.seed(SEED)
+   df = pd.read_csv('/home/yuan/ml/2018-1-15-essembling_example/data/input.csv')
+
    y = 1 * (df.cand_pty_affiliation == "REP")
    X = df.drop(["cand_pty_affiliation"], axis=1)
    X = pd.get_dummies(X, sparse=True)
@@ -19,6 +21,7 @@ def get_train_test(test_size=0.95):
 
    return train_test_split(X, y, test_size=test_size, random_state=SEED)
 
+'''
 xtrain, xtest, ytrain, ytest = get_train_test()
 # A look at the data
 print("\nExample data:")
@@ -28,3 +31,4 @@ df.cand_pty_affiliation.value_counts(normalize=True).plot(
    kind='bar', title='Share of No. donations'
 )
 plt.show()
+'''
